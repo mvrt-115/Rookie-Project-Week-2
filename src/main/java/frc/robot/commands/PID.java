87011ae35e4e99b849f9ff936ed90f8027c76b35
Vincent Zhao <vincentzhao12@github.com>
2022-11-06 18:32:16 -0800

    added back PID loop

--- src/main/java/frc/robot/commands/PID.java
+++ src/main/java/frc/robot/commands/PID.java
@@ -12,36 +12,74 @@ public class PID extends CommandBase {
   // create a field for the  Drivetrain subsystem
   /** Create the field variables for PID */
 
+  Drivetrain drivetrain;
+  double target;
+  double lastTime;
+  double area; 
+  double lastError;
+
+  final double kP = 0.1;
+  final double kI = 0;
+  final double kD = 0;
+
 
   /** Creates a new PID. */
   public PID(Drivetrain m, double targetMeters) {
      // Use addRequirements() here to declare subsystem dependencies.
     // initialize field variables
+
+    drivetrain = m;
+    target = targetMeters;
+    area = 0;
+    lastTime = 0;
+    lastError = 0;
+
+    addRequirements(m);
     
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    lastTime = Timer.getFPGATimestamp();
+    lastError = target - drivetrain.getPosition();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     /** Run the motor at 0.2 speed by using PID */
     // Calculate the error, time, derivative, and add to the integral
+    double error = target - drivetrain.getPosition();
+    double dt = Timer.getFPGATimestamp() - lastTime;
+    double de = error - lastError;
+    double derivative = de/dt;
+    
+    area += error * dt;
 
     // Calculate the output
+    double output = kP * error + kI * area + kD * derivative;
+
+    output = Math.min(Math.max(-1, output), 1);
+
+    // Run motor
+    drivetrain.setSpeeds(output);
+
+    // update last variables
+    lastTime = Timer.getFPGATimestamp();
+    lastError = target - drivetrain.getPosition();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     // stop the motor 
+    drivetrain.stop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return Math.abs(target - drivetrain.getPosition()) < 0.1;
   }
 }
